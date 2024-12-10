7fd5d5b949bfd4d3c4987318bb5c5864727fa2e0
Madasa-cell <adampaajarvi@gmail.com>
2024-12-10 16:04:07 +0100

    first commit

---
+++ fleetman/src/main/java/se/projekt/fleetman/FleetmanApplication.java
@@ -0,0 +1,13 @@
+package se.projekt.fleetman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmanApplication.class, args);
+	}
+
+}
