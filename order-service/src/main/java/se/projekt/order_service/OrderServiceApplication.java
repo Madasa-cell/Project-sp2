d643e1c25f86aa770820c6a3d6307f7effb847c4
elisan0107 <elisan0107@skola.goteborg.se>
2024-12-18 11:55:28 +0100

    add TODO in OrderServiceApplication

--- order-service/src/main/java/se/projekt/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/se/projekt/order_service/OrderServiceApplication.java
@@ -9,5 +9,12 @@ public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
+		// TODO:
+		// Create test order.
+		// Using information from CustomerService and ResturantService.
+		// We need to make api-call in the java code.
+		// And storing it in a new Order-object.
+		// Finally storing the order-object in our order database.
 
+		// We also need an "endpoint".
 }
